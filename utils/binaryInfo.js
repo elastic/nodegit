77bccbfb70573b4f90781d3663ca41b7fa87adc7
spacedragon <yulong.huang@elastic.co>
2018-12-13 09:36:29 +0800

    make libcurl and libssl static linked in linux

---
+++ utils/binaryInfo.js
@@ -0,0 +1,23 @@
+var versioning = require('node-pre-gyp/lib/util/versioning');
+var napi = require('node-pre-gyp/lib/util/napi');
+var fs = require('fs');
+var path = require('path');
+
+function binaryInfo(platform, arch) {
+
+    var package_json = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../package.json'), 'utf8'));
+
+    var options = {
+        target_platform: platform,
+        target_arch: arch
+    };
+    var napi_build_version = napi.get_napi_build_version_from_command_args([]);
+    var info = versioning.evaluate(package_json, options, napi_build_version);
+    console.log(info);
+    return info;
+};
+
+module.exports = binaryInfo;
+
+
+binaryInfo('windows', 'x86');
